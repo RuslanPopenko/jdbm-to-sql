0301e1fd4db43a7763542c09341e73b92333975d
RuslanPopenko <ruslanpopenko@ukr.net>
2017-01-16 15:03:22 +0200

    Database configuration.

--- src/main/java/com/imcode/tools/jdbmtosql/configurations/DatabaseConfig.java
+++ src/main/java/com/imcode/tools/jdbmtosql/configurations/DatabaseConfig.java
@@ -1,20 +1,72 @@
 package com.imcode.tools.jdbmtosql.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import javax.annotation.Resource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories("com.imcode.tools.jdbmtosql.repositories")
 public class DatabaseConfig {
 
-    private final Environment environment;
+    @Resource
+    private Environment environment;
 
-    @Autowired
-    public DatabaseConfig(Environment environment) {
-        this.environment = environment;
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(environment.getRequiredProperty("JdbcDriver"));
+        dataSource.setUrl(environment.getRequiredProperty("JdbcUrl"));
+        dataSource.setUsername(environment.getRequiredProperty("User"));
+        dataSource.setPassword(environment.getRequiredProperty("Password"));
+
+        return dataSource;
     }
 
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactoryBean.setPackagesToScan("com.imcode.tools.jdbmtosql.entities");
+
+        entityManagerFactoryBean.setJpaProperties(hibProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    private Properties hibProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.max_fetch_depth", 3);
+        properties.put("hibernate.jdbc.fetch_size", 50);
+        properties.put("hibernate.jdbc.batch_size", 10);
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.format_sql", true);
+        properties.put("hibernate.use_sql_comments", true);
+        properties.put("hibernate.dialect", environment.getRequiredProperty("HibernateDialect"));
+        properties.put("hibernate.pool_size", 10);
+        properties.put("current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+        properties.put("cache.provider_class", "org.hibernate.cache.internal.StandardQueryCache");
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("HibernateHbm2Ddl"));
+        return properties;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
 
 }
